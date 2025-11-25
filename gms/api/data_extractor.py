6f82b9c33805393375579bd4034a258eed4098e0
Cse18107 <cse18107@cemk.ac.in>
2025-11-25 11:18:23 +0000

    changed import place

--- gms/api/data_extractor.py
+++ gms/api/data_extractor.py
@@ -2,7 +2,6 @@ import json
 import os
 import sys
 
-import docx
 import frappe
 import google.generativeai as genai
 import openpyxl
@@ -10,6 +9,8 @@ from frappe.model.document import Document
 
 
 def extract_text_from_docx(file_path):
+	import docx
+
 	try:
 		doc = docx.Document(file_path)
 		full_text = []
