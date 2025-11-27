0f05778603e5bbddfe58717a53305ae4fc5209ad
Cse18107 <cse18107@cemk.ac.in>
2025-11-27 08:32:16 +0000

    removed data extraction code

--- gms/grant_planning/doctype/grant_milestones_type/grant_milestones_type.py
+++ gms/grant_planning/doctype/grant_milestones_type/grant_milestones_type.py
@@ -1,8 +1,9 @@
 # Copyright (c) 2025, Triveous and contributors
 # For license information, please see license.txt
 
+# import frappe
 from frappe.model.document import Document
 
 
-class GrantProjectQuarterlyUpdate(Document):
+class GrantMilestonesType(Document):
 	pass
