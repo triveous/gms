0421c746fc47dbf24f4ebfee6e9bd9264938fdcb
Farmaan Elahi <farmaan@triveous.com>
2025-12-03 18:04:44 +0530

    Added Shadcn

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -1,11 +1,12 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite"
 import proxyOptions from './proxyOptions';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tailwindcss()],
 	server: {
 		port: 8080,
 		host: '0.0.0.0',
