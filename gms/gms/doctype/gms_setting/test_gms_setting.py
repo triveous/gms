c93b462a445fe7fc241713138f673b756b11ccd7
Farmaan Elahi <farmaan@triveous.com>
2026-01-08 15:21:29 +0000

    Added AI Agent Setting

---
+++ gms/gms/doctype/gms_setting/test_gms_setting.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Triveous and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestGMSSetting(FrappeTestCase):
+	pass
