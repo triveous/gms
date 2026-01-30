5694016283204c1eff747beced24163435d7b3e7
Farmaan Elahi <farmaan@triveous.com>
2026-01-30 09:48:57 +0000

    Added langrpah checkpointer doc

---
+++ gms/ai/doctype/langgraph_write/langgraph_write.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Triveous and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("LangGraph Write", {
+// 	refresh(frm) {
+
+// 	},
+// });
