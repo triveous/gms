41750f858898523492a6137615e95ae943b57780
Farmaan Elahi <61222119+farmaan-e@users.noreply.github.com>
2026-02-25 18:47:00 +0530

    Logfire configured

--- gms/ai/__init__.py
+++ gms/ai/__init__.py
@@ -5,5 +5,24 @@ def init_ai():
 
     os.environ["GOOGLE_API_KEY"] = frappe.conf.get("google_api_key")
 
+def init_logfire():
+    import logfire
+    import os
+    import frappe
+    import traceback
+    
+    try:
+        # Enable langsmith tracing
+        os.environ['LANGSMITH_OTEL_ENABLED'] = 'true'
+        os.environ['LANGSMITH_OTEL_ONLY'] = 'true'
+        os.environ['LANGSMITH_TRACING'] = 'true'
+        os.environ['LOGFIRE_TOKEN'] = frappe.conf.get("logfire_token")
+            
+        logfire.configure(environment="prod")
+        print("Logfire Configured")    
+    except:
+        traceback.print_exception() 
 
+
+init_logfire()
 init_ai()
